78cfc36677cf38da69969edb11214bc5996b51ff
lasangaT <156019303+lasangaTurtle@users.noreply.github.com>
2024-03-25 15:51:55 -0400

    added most of the needed code

--- Class-Notes/JS/Intro to JavaScript/assignment 4/main.js
+++ Class-Notes/JS/Intro to JavaScript/assignment 4/main.js
@@ -10,5 +10,24 @@ function randomValueFromArray(array){
 
 let storytext = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 let insertx = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
-let inserty = ["the soup kitchen", "Disneyland", "the White House"]
-let insertz = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"]
+let inserty = ["the soup kitchen", "Disneyland", "the White House"];
+let insertz = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+
+randomize.addEventListener('click', result);
+
+function result() {
+
+  if(customName.value !== '') {
+    const name = customName.value;
+
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(300);
+    const temperature =  Math.round(94);
+
+  }
+
+  story.textContent = ;
+  story.style.visibility = 'visible';
+}
