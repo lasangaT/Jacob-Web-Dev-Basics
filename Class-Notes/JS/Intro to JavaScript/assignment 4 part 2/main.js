b1b437b893bee12f12904c9f7a377558460a37d0
lasangaT <156019303+lasangaTurtle@users.noreply.github.com>
2024-04-01 15:29:45 -0400

    added darken function to main image

--- Class-Notes/JS/Intro to JavaScript/assignment 4 part 2/main.js
+++ Class-Notes/JS/Intro to JavaScript/assignment 4 part 2/main.js
@@ -1,21 +1,33 @@
-const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg'];
-const altText = {
-  'pic1.jpg': 'Closeup of a human eye',
-  'pic2.jpg': 'Beautiful landscape',
-  'pic3.jpg': 'Colorful flowers'
-};
+document.addEventListener('DOMContentLoaded', function() {
+  const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+  const altText = {
+    'pic1.jpg': 'Closeup of a human eye',
+    'pic2.jpg': 'Beautiful landscape',
+    'pic3.jpg': 'Colorful flowers',
+    'pic4.jpg': 'Mountain view',
+    'pic5.jpg': 'Ocean sunset'
+  };
 
-images.forEach(filename => {
-  const img = document.createElement('img');
-  img.setAttribute('src', 'images/' + filename);
-  img.setAttribute('alt', altText[filename]);
-  img.addEventListener('click', function() {
-    displayedImage.setAttribute('src', 'images/' + filename);
-    displayedImage.setAttribute('alt', altText[filename]);
+  const displayedImage = document.querySelector('.displayed-img');
+  const thumbBar = document.getElementById('thumbBar');
+  const btnDarken = document.querySelector('.dark');
+
+  images.forEach(filename => {
+    const img = document.createElement('img');
+    img.setAttribute('src', 'images/' + filename);
+    img.setAttribute('alt', altText[filename]);
+    img.addEventListener('click', function() {
+      displayedImage.setAttribute('src', 'images/' + filename);
+      displayedImage.setAttribute('alt', altText[filename]);
+    });
+    thumbBar.appendChild(img);
   });
-  thumbBar.appendChild(img);
+
+  if (btnDarken) {
+    btnDarken.addEventListener('click', function() {
+      console.log('Darken button clicked');
+      displayedImage.style.filter = 'brightness(50%)';
+      displayedImage.classList.toggle('dark');
+    });
+  }
 });
-
-btn.addEventListener('click', function() {
-    displayedImage.classList.toggle('dark');
-  });
